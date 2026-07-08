d0b0cb5af7b10d54240742550728e049d612021f
MegaZupik <82896583+MegaZupik@users.noreply.github.com>
2026-07-08 17:54:57 +0300

    Update pokemon.user.js

--- pokemon.user.js
+++ pokemon.user.js
@@ -1,834 +1,187 @@
 // ==UserScript==
 // @name         MosWar Robot Button
 // @namespace    https://www.moswar.ru/
-// @version      5.0
-// @description  Robot mech + talents button
+// @version      6.0
+// @description  Robot mech + talents
 // @match        https://www.moswar.ru/*
 // @grant        none
 // @run-at       document-end
 // ==/UserScript==
 
-
 (function(){
-
 'use strict';
 
+const ID='mw-robot-button';
+const POS='mw_robot_button_position';
+let notes=[];
 
-const BUTTON_ID = 'mw-robot-button';
-const POS_KEY = 'mw_robot_button_position';
+if(document.getElementById(ID))return;
 
-
-
-if(document.getElementById(BUTTON_ID))
-    return;
-
-
-
-
-
-function sleep(ms){
-
-    return new Promise(
-        resolve=>setTimeout(resolve,ms)
-    );
-
-}
-
-
-
-
-
-
+function sleep(ms){return new Promise(r=>setTimeout(r,ms));}
 
 function showNotify(text){
-
-
-    let old=document.getElementById(
-        'mw-robot-notify'
-    );
-
-
-    if(old)
-        old.remove();
-
-
-
-
-    let div=document.createElement('div');
-
-
-    div.id='mw-robot-notify';
-
-
-    div.innerHTML=text;
-
-
-
-    Object.assign(div.style,{
-
+    let d=document.createElement('div');
+    d.innerHTML=text;
+    Object.assign(d.style,{
         position:'fixed',
-
-        top:'50%',
-
         left:'50%',
-
-        transform:'translate(-50%,-50%)',
-
+        top:(80+notes.length*60)+'px',
+        transform:'translateX(-50%)',
         background:'rgba(0,0,0,.85)',
-
         color:'#fff',
-
-        padding:'15px 25px',
-
-        borderRadius:'12px',
-
+        padding:'12px 20px',
+        borderRadius:'10px',
         fontSize:'18px',
-
         zIndex:1000000,
-
         textAlign:'center'
-
     });
-
-
-
-
-    document.body.appendChild(div);
-
-
-
-
+    document.body.appendChild(d);
+    notes.push(d);
     setTimeout(()=>{
-
-        div.remove();
-
+        d.remove();
+        notes=notes.filter(x=>x!==d);
+        notes.forEach((x,i)=>x.style.top=(80+i*60)+'px');
     },2000);
-
-
-
 }
-
-
-
-
-
-
-
-
 
 async function sendPost(url,body){
-
-
-
-    console.log(
-        'SEND:',
-        url,
-        body
-    );
-
-
-
-
-    let response =
-    await fetch(
-
-        url,
-
-        {
-
+    console.log('SEND',url,body);
+    let r=await fetch(url,{
         method:'POST',
-
         credentials:'include',
-
         headers:{
-
-
-            'Content-Type':
-            'application/x-www-form-urlencoded; charset=UTF-8',
-
-
-            'X-Requested-With':
-            'XMLHttpRequest'
-
-
+            'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',
+            'X-Requested-With':'XMLHttpRequest'
         },
-
-
-        body:body
-
-
-        }
-
-    );
-
-
-
-
-    let text =
-    await response.text();
-
-
-
-    console.log(
-        'ANSWER:',
-        text
-    );
-
-
-
-
-    try{
-
-
-        return JSON.parse(text);
-
-
-    }
-    catch(e){
-
-
-        return {};
-
-
-    }
-
-
+        body
+    });
+    let t=await r.text();
+    console.log('ANSWER',t);
+    try{return JSON.parse(t)}catch(e){return {}}
 }
 
+function hasError(data){
+    return data.error && data.error.length;
+}
 
-
-
-
-
-
-
-
-async function activateNatal(type,num){
-
-
-
-    let data =
-    await sendPost(
-
+async function natal(type,num){
+    let data=await sendPost(
         '/natal2026/',
-
-
-        'action=activate-talant&type='+
-        type+
-        '&ajax=1&__referrer=%2Fnatal2026%2F&return_url=%2Fnatal2026%2F'
-
+        'action=activate-talant&type='+type+'&ajax=1&__referrer=%2Fnatal2026%2F&return_url=%2Fnatal2026%2F'
     );
-
-
-
-
-    console.log(
-        'NATAL '+num,
-        data
-    );
-
-
-
-
-
-    if(data.result !== 1){
-
-
-        showNotify(
-            'Талант №'+num+' в откате'
-        );
-
-
-        return data;
-
-
+    if(hasError(data)){
+        showNotify('Талант №'+num+' в откате');
+        return false;
     }
-
-
-
-
-
-
-
-    if(data.code==='revive'){
-
-
-        showNotify(
-            'Вы получили бессмертие на 1 ход<br>Код: revive'
-        );
-
-
-    }
-
-
-
-    else if(data.code==='passive2'){
-
-
-        showNotify(
-            'Вы получили немного статов<br>Код: passive2'
-        );
-
-
-    }
-
-
-
-    else if(data.code){
-
-
-        showNotify(
-            'Код: '+data.code
-        );
-
-
-    }
-
-
-
-    return data;
-
-
+    if(data.code==='revive')
+        showNotify('Вы получили бессмертие на 1 ход<br>Код: revive');
+    else if(data.code==='passive2')
+        showNotify('Вы получили немного статов<br>Код: passive2');
+    else if(data.code)
+        showNotify('Код: '+data.code);
+    return true;
 }
 
+async function runRobot(){
+    showNotify('Робот запущен');
 
-
-
-
-
-
-
-
-async function doAll(){
-
-
-
-console.log(
-    'ROBOT START'
-);
-
-
-
-
-try{
-
-
-    let mech =
-    await sendPost(
-
+    let mech=await sendPost(
         '/mech/',
-
-
         'action=overcharge&__referrer=%2Fmech%2F&return_url=%2Fmech%2F'
-
     );
 
-
-
-
-
-
-    if(mech.result !== 1){
-
-
-        showNotify(
-            'Робот в откате'
-        );
-
-
+    if(hasError(mech)){
+        showNotify('Робот в откате');
         return;
-
-
     }
 
+    await sleep(50);
 
-
-
+    await natal('talants',2);
 
     await sleep(50);
 
-
-
-
-
-
-    await activateNatal(
-
-        'talants',
-
-        2
-
-    );
-
-
-
-
-
-
-    await sleep(50);
-
-
-
-
-
-
-    await activateNatal(
-
-        'abils',
-
-        3
-
-    );
-
-
-
-
-
-
-
+    await natal('abils',3);
 }
-catch(e){
-
-
-    console.error(
-        'ROBOT ERROR',
-        e
-    );
-
-
-}
-
-
-
-console.log(
-    'ROBOT END'
-);
-
-
-
-}
-
-
-
-
-
-
-
-
 
 function createButton(){
+    let b=document.createElement('div');
+    b.id=ID;
 
+    let p=JSON.parse(localStorage.getItem(POS)||'null');
 
+    Object.assign(b.style,{
+        position:'fixed',
+        left:p?p.left:'20px',
+        top:p?p.top:'80px',
+        width:'65px',
+        height:'65px',
+        background:'#333',
+        border:'2px solid #aaa',
+        borderRadius:'12px',
+        zIndex:999999,
+        display:'flex',
+        alignItems:'center',
+        justifyContent:'center',
+        touchAction:'none',
+        userSelect:'none'
+    });
 
-let btn =
-document.createElement('div');
+    let img=document.createElement('img');
+    img.src='/@/images/loc/robot/robot_3.png';
+    img.width=55;
+    img.height=55;
+    b.appendChild(img);
 
+    let drag=false,moved=false,dx=0,dy=0;
 
-btn.id=BUTTON_ID;
+    function start(x,y){
+        drag=true;
+        moved=false;
+        dx=x-b.offsetLeft;
+        dy=y-b.offsetTop;
+    }
 
+    function move(x,y){
+        if(!drag)return;
+        if(Math.abs(x-b.offsetLeft)>5||Math.abs(y-b.offsetTop)>5)moved=true;
+        b.style.left=(x-dx)+'px';
+        b.style.top=(y-dy)+'px';
+    }
 
+    function end(){
+        if(!drag)return;
+        drag=false;
+        localStorage.setItem(POS,JSON.stringify({
+            left:b.style.left,
+            top:b.style.top
+        }));
+    }
 
+    b.addEventListener('mousedown',e=>start(e.clientX,e.clientY));
+    document.addEventListener('mousemove',e=>move(e.clientX,e.clientY));
+    document.addEventListener('mouseup',end);
 
+    b.addEventListener('touchstart',e=>{
+        let t=e.touches[0];
+        start(t.clientX,t.clientY);
+    },{passive:false});
 
-let left='20px';
+    b.addEventListener('touchmove',e=>{
+        let t=e.touches[0];
+        move(t.clientX,t.clientY);
+        e.preventDefault();
+    },{passive:false});
 
-let top='80px';
+    b.addEventListener('touchend',end);
 
+    b.onclick=()=>{
+        if(!moved)runRobot();
+    };
 
-
-
-
-let saved =
-localStorage.getItem(
-    POS_KEY
-);
-
-
-
-
-
-if(saved){
-
-
-try{
-
-
-let p =
-JSON.parse(saved);
-
-
-left=p.left;
-
-top=p.top;
-
-
-
+    document.body.appendChild(b);
 }
-catch(e){}
-
-
-
-}
-
-
-
-
-
-
-
-
-Object.assign(btn.style,{
-
-
-position:'fixed',
-
-left:left,
-
-top:top,
-
-width:'65px',
-
-height:'65px',
-
-background:'#333',
-
-border:'2px solid #aaa',
-
-borderRadius:'12px',
-
-zIndex:999999,
-
-display:'flex',
-
-alignItems:'center',
-
-justifyContent:'center',
-
-touchAction:'none',
-
-userSelect:'none'
-
-
-});
-
-
-
-
-
-
-
-let img =
-document.createElement('img');
-
-
-
-img.src =
-'/@/images/loc/robot/robot_3.png';
-
-
-
-img.width=55;
-
-img.height=55;
-
-
-
-btn.appendChild(img);
-
-
-
-
-
-
-
-
-
-let dragging=false;
-
-let moved=false;
-
-let dx=0;
-
-let dy=0;
-
-
-
-
-
-
-
-
-function startDrag(x,y){
-
-
-dragging=true;
-
-moved=false;
-
-
-dx=x-btn.offsetLeft;
-
-dy=y-btn.offsetTop;
-
-
-}
-
-
-
-
-
-
-
-function moveDrag(x,y){
-
-
-if(!dragging)
-return;
-
-
-
-
-if(
-
-Math.abs(x-btn.offsetLeft)>5 ||
-
-Math.abs(y-btn.offsetTop)>5
-
-)
-
-moved=true;
-
-
-
-
-
-btn.style.left =
-(x-dx)+'px';
-
-
-
-btn.style.top =
-(y-dy)+'px';
-
-
-
-}
-
-
-
-
-
-
-
-
-function endDrag(){
-
-
-
-if(!dragging)
-return;
-
-
-
-dragging=false;
-
-
-
-
-localStorage.setItem(
-
-POS_KEY,
-
-
-JSON.stringify({
-
-left:btn.style.left,
-
-top:btn.style.top
-
-
-})
-
-
-);
-
-
-
-}
-
-
-
-
-
-
-
-
-
-btn.addEventListener(
-
-'mousedown',
-
-e=>{
-
-startDrag(
-e.clientX,
-e.clientY
-);
-
-}
-
-);
-
-
-
-
-
-
-document.addEventListener(
-
-'mousemove',
-
-e=>{
-
-moveDrag(
-e.clientX,
-e.clientY
-);
-
-}
-
-);
-
-
-
-
-
-
-document.addEventListener(
-
-'mouseup',
-
-endDrag
-
-);
-
-
-
-
-
-
-
-
-
-btn.addEventListener(
-
-'touchstart',
-
-e=>{
-
-
-let t=e.touches[0];
-
-
-startDrag(
-t.clientX,
-t.clientY
-);
-
-
-},
-
-{passive:false}
-
-);
-
-
-
-
-
-
-
-btn.addEventListener(
-
-'touchmove',
-
-e=>{
-
-
-let t=e.touches[0];
-
-
-moveDrag(
-t.clientX,
-t.clientY
-);
-
-
-
-e.preventDefault();
-
-
-},
-
-{passive:false}
-
-);
-
-
-
-
-
-
-
-btn.addEventListener(
-
-'touchend',
-
-endDrag
-
-);
-
-
-
-
-
-
-
-
-btn.onclick=()=>{
-
-
-if(moved)
-return;
-
-
-
-doAll();
-
-
-};
-
-
-
-
-
-
-
-
-document.body.appendChild(btn);
-
-
-
-}
-
-
-
-
-
-
 
 createButton();
-
-
 
 })();
